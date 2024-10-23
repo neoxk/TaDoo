f8e74f56127c193b7a7ef40e1a88dad9bebcaf08
nxk <neo.kirbis@gmail.com>
2024-10-23 19:16:35 +0200

    Adjusted Board model

--- frontend/src/models/Board.ts
+++ frontend/src/models/Board.ts
@@ -1,3 +1,13 @@
 export class Board {
-  constructor(public name: String, public id?: Number) {}
+  private _board_id: number;
+  public name: string;
+  
+  constructor(board_id: number, name: string) {
+    this._board_id = board_id;
+    this.name = name;
+  }
+
+  get id() {
+    return this._board_id;
+  }
 }
