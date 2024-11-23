d7502be2438ad5773e15018e58ad4d439c59060c
NXK <neo.kirbis@gmail.com>
2024-11-23 19:24:14 +0100

    feat: Implemented service calls and onBlur on EditableText causes update service call

--- frontend/src/app.tsx
+++ frontend/src/app.tsx
@@ -5,34 +5,52 @@ import { Canvas } from "./components/common/Canvas.tsx";
 import { BoardService } from "./services/BoardService.ts";
 
 export function App() {
-    const [getBoards, setBoards] = useState<Board[]>([]);
-    const [selectedBoard, setSelectedBoard] = useState<Board | null>(null);
+  const [getBoards, setBoards] = useState<Board[]>([]);
+  const [selectedBoard, setSelectedBoard] = useState<Board | null>(null);
 
-    const boardService = new BoardService();
+  const boardService = new BoardService();
 
-    useEffect(() => {
-        const fetchBoards = async () => {
-            const boards = await boardService.getBoards();
-            setBoards(boards);
-            if (boards.length > 0) setSelectedBoard(boards[0]); // default prikaz : prvi board
-        };
-        fetchBoards();
-    }, []);
-
-    const handleBoardSelect = (board: Board) => {
-        setSelectedBoard(board);
+  useEffect(() => {
+    const fetchBoards = async () => {
+      const boards = await boardService.getBoards();
+      setBoards(boards);
+      if (boards.length > 0) setSelectedBoard(boards[0]); // default prikaz : prvi board
     };
+    fetchBoards();
+  }, []);
+
+  const handleBoardSelect = (board: Board) => {
+    setSelectedBoard(board);
+  };
+
+  const handleBoardRename = (newName: string) => {
+    if (selectedBoard) {
+      boardService
+        .updateBoard({
+          ...selectedBoard,
+          name: newName,
+        })
+        .then((board) => {
+          setBoards((prevBoards) =>
+            prevBoards.map((prevBoard) =>
+              prevBoard.board_id === board.board_id ? board : prevBoard
+            )
+          );
+        });
+    }
+  };
 
   return (
     <div class="h-screen flex">
-
-        <Sidebar boards={getBoards} onSelectBoard={handleBoardSelect} />
-        {selectedBoard ? (
-            <Canvas shownBoard={selectedBoard} />
-        ) : (
-            <p>Select a Board!</p>
-            )
-        }
+      <Sidebar boards={getBoards} onSelectBoard={handleBoardSelect} />
+      {selectedBoard ? (
+        <Canvas
+          shownBoard={selectedBoard}
+          handleBoardRename={(newName) => handleBoardRename(newName)}
+        />
+      ) : (
+        <p>Select a Board!</p>
+      )}
     </div>
   );
 }
--- frontend/src/components/tasks/TaskCard.tsx
+++ frontend/src/components/tasks/TaskCard.tsx
@@ -1,5 +1,6 @@
 import { Tag } from "../../models/Tag";
-import {Color, Icon} from "../../types/types";
+import { Task } from "../../models/Task";
+import { Color, Icon } from "../../types/types";
 import { Button } from "../common/Button";
 import { Checkbox } from "../common/Checkbox";
 import { EditableText } from "../common/EditableText";
@@ -7,31 +8,54 @@ import { TagView } from "./TagView";
 // import {useState} from "preact/hooks";
 
 export interface TaskCardProps {
+  id: number;
   name: string;
   tags: Tag[];
   onRemove: () => void;
   onTagColorChange: (tagName: string, newColor: Color) => void;
+  handleTaskChange: (updatedTask: Task) => void;
 }
 
-export const TaskCard = ({ name, tags, onRemove, onTagColorChange }: TaskCardProps) => {
+export const TaskCard = ({
+  id,
+  name,
+  tags,
+  onRemove,
+  onTagColorChange,
+  handleTaskChange,
+}: TaskCardProps) => {
+  // const [taskTags, setTaskTags] = useState(tags);
 
-    // const [taskTags, setTaskTags] = useState(tags);
-
-    return (
-        <div class="flex justify-between mt-4 align-center">
-          <div>
-            <div class="inline-flex items-center gap-2">
-              <Checkbox checked={false} />
-              <EditableText text={name} handleChange={() => {}} />
-              {tags.map((tag) => (
-                <TagView key={tag.name} name={tag.name} color={tag.color} onChangeColor={(newColor) => onTagColorChange(tag.name, newColor)} />
-              ))}
-            </div>
-          </div>
-          <div class="flex gap-2 self-center">
-            <Button onClick={onRemove} size="small" iconName={Icon.trash} />
-            <Button onClick={() => {}} size="small" iconName={Icon.send} />
-          </div>
+  return (
+    <div class="flex justify-between mt-4 align-center">
+      <div>
+        <div class="inline-flex items-center gap-2">
+          <Checkbox checked={false} />
+          <EditableText
+            text={name}
+            handleChange={(newText) =>
+              handleTaskChange({
+                _task_id: id,
+                name: newText,
+                done: false,
+                tags,
+              })
+            }
+          />
+          {tags.map((tag) => (
+            <TagView
+              key={tag.name}
+              name={tag.name}
+              color={tag.color}
+              onChangeColor={(newColor) => onTagColorChange(tag.name, newColor)}
+            />
+          ))}
         </div>
+      </div>
+      <div class="flex gap-2 self-center">
+        <Button onClick={onRemove} size="small" iconName={Icon.trash} />
+        <Button onClick={() => {}} size="small" iconName={Icon.send} />
+      </div>
+    </div>
   );
 };
